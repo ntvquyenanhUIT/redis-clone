2016582cd845c90db506323937efd172965d4c06
tomnguyn <tomnguyn@QuyenAnh.>
2025-08-06 12:43:51 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -16,21 +17,41 @@ func main() {
 
 	//Uncomment this block to pass the first stage
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
+
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error responding to PING")
-		os.Exit(1)
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		_, err := conn.Read(buf)
+
+		if err != nil {
+
+			if err == io.EOF {
+				break
+			}
+
+			fmt.Println("Error reading from connection ", err)
+			os.Exit(1)
+		}
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error responding to client")
+			break
+		}
+
 	}
 
 }
